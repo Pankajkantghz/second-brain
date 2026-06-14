136c594072b082d7a99cb749808dc25a73797a93
Pankaj Yadav <pankaj.py96810@gmail.com>
2026-06-14 15:52:04 +0530

    deploy frontend fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import DashBoard from "./Pages/DashBoard";
 import Signin from "./Pages/Signin";
@@ -9,6 +9,9 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Default Route */}
+        <Route path="/" element={<Navigate to="/signin" replace />} />
+
         <Route path="/signup" element={<Signup />} />
 
         <Route path="/signin" element={<Signin />} />
@@ -16,6 +19,9 @@ const App = () => {
         <Route path="/dashboard" element={<DashBoard />} />
 
         <Route path="/share/:shareLink" element={<SharedBrain />} />
+
+        {/* Fallback Route */}
+        <Route path="*" element={<Navigate to="/signin" replace />} />
       </Routes>
     </BrowserRouter>
   );
